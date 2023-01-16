b0c7d8083a039bc1099c013ea755803f18cdd765
Tatyana Dolgova <kylikovatatyana1993@mail.ru>
2023-01-16 08:59:23 +0500

    add js code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log(1);
+window.onload = function() {
+
+  //Tags
+  addTagsClickHandler();
+};
+
+const addTagsClickHandler = () => {
+  document.querySelector('.strategies__tags').addEventListener('click', (e) => {
+    if(e.target.classList.contains('tag')) {
+      let clikedTag = e.target;
+      removeSelectedTads();
+    }
+  });
+};
+
+const removeSelectedTads= () => {
+  let tags = document.querySelectorAll('.strategies__tags .tag');
+  console.log(tags);
+};

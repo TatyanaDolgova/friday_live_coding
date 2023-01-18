a039a9afe3731630759029630e0709b25c53d687
Tatyana Dolgova <kylikovatatyana1993@mail.ru>
2023-01-18 10:16:31 +0500

    add js for strategy

--- src/index.js
+++ src/index.js
@@ -10,6 +10,11 @@ const addTagsClickHandler = () => {
       let clickedTag = e.target;
       removeSelectedTads();
       selectClickedTag(clickedTag);
+      if(clickedTag.innerText === 'All') {
+        showAllStrategies();
+      } else {
+        filterStrategyBySelectedTag(clickedTag.innerText);
+      }
     }
   });
 };
@@ -26,3 +31,23 @@ const selectClickedTag = (clickedTag) => {
   clickedTag.classList.add('tag_selected');
   clickedTag.classList.remove('tag_bordered');
 }; 
+
+const showAllStrategies = () => {
+  let strategies = document.querySelectorAll('.strategy-wrapper .strategy');
+  strategies.forEach(strategy => {
+    strategy.classList.remove('strategy_hidden');
+    
+    });
+};
+
+const filterStrategyBySelectedTag = (selectedTag) => {
+  let strategies = document.querySelectorAll('.strategy-wrapper .strategy');
+  strategies.forEach(strategy => {
+    strategy.classList.add('strategy_hidden');
+    strategy.querySelectorAll('.tag').forEach(tag => {
+      if(tag.innerText === selectedTag) {
+        strategy.classList.remove('strategy_hidden');
+      }
+    });
+  });
+};
